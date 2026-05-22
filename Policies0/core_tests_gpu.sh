c622b35376a6becf5f16817f9a5516a65aae4473
CatherineMAbraham <catherinemabraham@gmail.com>
2026-05-22 09:52:31 +0100

    - Tests to run

--- Policies0/core_tests_gpu.sh
+++ Policies0/core_tests_gpu.sh
@@ -6,8 +6,8 @@
 #SBATCH --ntasks=1            # 4 agents total
 #SBATCH --cpus-per-task=1      # 4 CPUs per agent
 #SBATCH --mem=8G              # 8GB RAM per agent
-#SBATCH --time=96:00:00
-#SBATCH --array=1-6
+#SBATCH --time=10:00:00
+#SBATCH --array=1-18
 #SBATCH --output=out_%A_%a.out
 #SBATCH --error=err_%A_%a.err
 
@@ -19,8 +19,8 @@ source activate softsurg
 TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
 PARAM_LINE=$(sed -n "${TASK_ID}p" params.csv)
 #PARAM_LINE=$(sed -n "$((SLURM_ARRAY_TASK_ID))p" params2.csv)
-IFS=',' read -r THRESH_POS THRESH_ORI ACTION_TYPE<<< "$PARAM_LINE"
+IFS=',' read -r THRESH_POS THRESH_ORI ACTION_TYPE SEED<<< "$PARAM_LINE  "
 
 # Run the script
 #srun --export=ALL 
-python td3_v0.py --threshold_pos $THRESH_POS --threshold_ori $THRESH_ORI --action_type $ACTION_TYPE --seed 1 --ran $TASK_ID
+python td3_v0.py --threshold_pos $THRESH_POS --threshold_ori $THRESH_ORI --action_type $ACTION_TYPE --seed $SEED --ran $TASK_ID
--- Policies0/params.sh
+++ Policies0/params.sh
@@ -3,14 +3,17 @@
 > params.csv
 
 # First three pairs with both fouractions and euler
-pos_ori_pairs_both=("0.00005 0.05" "0.0001 0.1" "0.0002 0.2")
+pos_ori_pairs_both=("0.000025 0.25" "0.00005 0.5" "0.0001 1")
 for pair in "${pos_ori_pairs_both[@]}"; do
   pos=$(echo $pair | cut -d' ' -f1)
   ori=$(echo $pair | cut -d' ' -f2)
   for act in euler fouractions; do
-      echo "$pos,$ori,$act" >> params.csv
+      for seed in {1..3}; do
+          echo "$pos,$ori,$act,$seed" >> params.csv
+      done
   done
 done
+     
 
 # Last two pairs with only euler
 # pos_ori_pairs_euler=("0.0005 1" "0.002 0.25")
