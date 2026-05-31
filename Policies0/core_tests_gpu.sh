24346acf4b5ebc7752385f0c37180735e84008a0
CatherineMAbraham <catherinemabraham@gmail.com>
2026-05-31 13:48:38 +0100

    testing new thresholds between 0.5mm and 0.1mm

--- Policies0/core_tests_gpu.sh
+++ Policies0/core_tests_gpu.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=1      # 4 CPUs per agent
 #SBATCH --mem=8G              # 8GB RAM per agent
 #SBATCH --time=10:00:00
-#SBATCH --array=1-5
+#SBATCH --array=1-3
 #SBATCH --output=out_%A_%a.out
 #SBATCH --error=err_%A_%a.err
 
