68980a1501c9a6a0a94d17b2c11fdceb97e89a66
CatherineMAbraham <catherinemabraham@gmail.com>
2026-06-12 07:18:57 +0100

    running on cpu

---
+++ Policies0/compare_run cpu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --ntasks=1            # 4 agents total
+#SBATCH --cpus-per-task=1     # 4 CPUs per agent
+#SBATCH --mem=20G              # 8GB RAM per agent
+#SBATCH --time=12:00:00
+#SBATCH --array=1-20
+#SBATCH --output=out_%A_%a.out
+#SBATCH --error=err_%A_%a.err
+
+module load Anaconda3/2024.02-1
+
+source activate softsurg
+
+# Read the correct line from params_all.csv
+TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
+PARAM_LINE=$(sed -n "${TASK_ID}p" params_all.csv)
+IFS=',' read -r MODEL REWARD SEED <<< "$PARAM_LINE"
+
+# Run the script
+srun --export=ALL python alg_compare.py --threshold_pos 0.0005 --threshold_ori 0.5 --action_type 'euler' --seed $SEED --reward $REWARD --model $MODEL --ran $TASK_ID
