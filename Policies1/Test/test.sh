1fd57eb0d9c3bb56bd718bcaea74ca5067a46cf2
CatherineMAbraham <catherinemabraham@gmail.com>
2026-06-12 17:44:34 +0100

    forgot - in :-1

--- Policies1/Test/test.sh
+++ Policies1/Test/test.sh
@@ -13,7 +13,7 @@ module load Anaconda3/2024.02-1
 
 source activate softsurg
 # Read the correct line from params_curr_compare.csv
-TASK_ID=${SLURM_ARRAY_TASK_ID:1}
+TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
 PARAM_LINE=$(sed -n "${TASK_ID}p" tests.csv)
 IFS=',' read -r FILE YOUNGS_MODULUS EXPERT <<< "$PARAM_LINE"
 echo "Running test with: Young's Modulus=$YOUNGS_MODULUS, VTK File=$FILE, Expert Trajectory=$EXPERT"
