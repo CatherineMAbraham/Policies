97d4ef06b8f7cd984aedece13f1b413927eba65d
CatherineMAbraham <catherinemabraham@gmail.com>
2026-06-14 14:43:45 +0100

    testing new vtk files

--- Policies1/Test/test.sh
+++ Policies1/Test/test.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1            # 4 agents total
 #SBATCH --cpus-per-task=1      # 4 CPUs per agent
 #SBATCH --mem=8G              # 8GB RAM per agent
-#SBATCH --array=1-20
+#SBATCH --array=1-4
 #SBATCH --time=1:00:00
 #SBATCH --output=out_%A_%a.out
 
@@ -13,9 +13,10 @@ module load Anaconda3/2024.02-1
 
 source activate softsurg
 # Read the correct line from params_curr_compare.csv
-TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
+TASK_ID=${SLURM_ARRAY_TASK_ID:-16}
 PARAM_LINE=$(sed -n "${TASK_ID}p" tests.csv)
 IFS=',' read -r FILE YOUNGS_MODULUS EXPERT <<< "$PARAM_LINE"
 echo "Running test with: Young's Modulus=$YOUNGS_MODULUS, VTK File=$FILE, Expert Trajectory=$EXPERT"
 # Run the script 
-srun --export=ALL python force_from_expert.py --threshold_pos 0.0001 --threshold_ori 0.5 --maxforce 4 --softtissue soft --youngs_modulus $YOUNGS_MODULUS --vtk_file $FILE --expert $EXPERT --log 1
+#srun --export=ALL 
+python force_from_expert.py --threshold_pos 0.0001 --threshold_ori 0.5 --maxforce 4 --softtissue soft --youngs_modulus $YOUNGS_MODULUS --vtk_file $FILE --expert $EXPERT --log 0
