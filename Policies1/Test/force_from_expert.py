4a9e573b88afce19ed4e155a56515b53af48a872
CatherineMAbraham <catherinemabraham@gmail.com>
2026-06-14 14:57:47 +0100

    testing vtk with 1e5 ym

--- Policies1/Test/force_from_expert.py
+++ Policies1/Test/force_from_expert.py
@@ -188,7 +188,7 @@ if __name__ == "__main__":
         args = parser.parse_args()
 
         if args.log == 1:
-                wandb.init(project="meshconvergence", name=f"{args.vtk_file}_{args.expert}_{args.youngs_modulus}",tags=[args.expert,'meshtest'])
+                wandb.init(project="meshconvergence", name=f"{args.vtk_file}_{args.expert}_{args.youngs_modulus}",tags=[args.expert,'meshtest5'])
 
         multiple_envs(
                 model_path=args.model_path,
