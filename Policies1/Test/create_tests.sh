f6dc71e0cd2bfcc5d6e2b208e5b0fdb5a9c0069d
CatherineMAbraham <catherinemabraham@gmail.com>
2026-06-14 15:14:13 +0100

    - testing other vtk files

--- Policies1/Test/create_tests.sh
+++ Policies1/Test/create_tests.sh
@@ -4,7 +4,7 @@
 > tests.csv
 
 Generate combinations of tissue option, number of springs
-for file in "rect0001.vtk" "rect000125.vtk" "rect00025.vtk" "rect0005.vtk" "rect001.vtk" "rect00125.vtk" "rect0025.vtk" "rect005.vtk"; do
+for file in "rect0009.vtk" "rect0008.vtk" "rect0007.vtk" "rect0006.vtk"; do
     for ym in "1e5"; do
         for expert in "trajectory_2"; do
             echo "$file,$ym,$expert" >> tests.csv
