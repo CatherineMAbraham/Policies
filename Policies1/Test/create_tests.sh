a8c5d7f6294172082151a13236eae23afa8f0b76
CatherineMAbraham <catherinemabraham@gmail.com>
2026-06-18 07:20:55 +0100

    tests low

--- Policies1/Test/create_tests.sh
+++ Policies1/Test/create_tests.sh
@@ -6,7 +6,7 @@
 #Generate combinations of tissue option, number of springs
 for file in "rect0005.vtk" "rect0006.vtk" "rect0007.vtk" "rect0008.vtk" "rect0009.vtk" "rect001.vtk" "rect00125.vtk" "rect0025.vtk" "rect005.vtk" "rect01.vtk" "None"; do
     for ym in "1e6"; do
-        for expert in "Expert_2_actions"; do
+        for expert in "Expert_3_actions"; do
             echo "$file,$ym,$expert" >> tests.csv
         done
     done
