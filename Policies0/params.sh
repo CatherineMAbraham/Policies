957d85874f340fe10a609e50111058796372941f
CatherineMAbraham <catherinemabraham@gmail.com>
2026-06-30 14:16:19 +0100

    - running pos only but forgot before to take threshold_ori out of sbatch script

--- Policies0/params.sh
+++ Policies0/params.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
-> params.csv
+> params_pos.csv
 
 # First three pairs with both fouractions and euler
-pos_ori_pairs_both=("0.001 1" "0.0005 0.5" "0.0001 0.1")
-for pair in "${pos_ori_pairs_both[@]}"; do
-  pos=$(echo $pair | cut -d' ' -f1)
-  ori=$(echo $pair | cut -d' ' -f2)
-  for act in fouractions; do
-      for seed in {1..10}; do
-          echo "$pos,$ori,$act,$seed" >> params.csv
-      done
-  done
-done
-
-pos_ori_pairs_both=("0.00015 0.15")
-for pair in "${pos_ori_pairs_both[@]}"; do
-  pos=$(echo $pair | cut -d' ' -f1)
-  ori=$(echo $pair | cut -d' ' -f2)
-  for act in euler; do
-      for seed in {1..10}; do
-          echo "$pos,$ori,$act,$seed" >> params.csv
-      done
-  done
-done
-
-# Last two pairs with only euler
 pos=("0.001" "0.0005" "0.0001")
 for pair in "${pos[@]}"; do
   pos=$(echo $pair | cut -d' ' -f1)
   for act in pos_only; do
       for seed in {1..10}; do
-          echo "$pos,$act,$seed" >> params.csv
+          echo "$pos,$act,$seed" >> params_pos.csv
       done
   done
 done
+
+# pos_ori_pairs_both=("0.00015 0.15")
+# for pair in "${pos_ori_pairs_both[@]}"; do
+#   pos=$(echo $pair | cut -d' ' -f1)
+#   ori=$(echo $pair | cut -d' ' -f2)
+#   for act in euler; do
+#       for seed in {1..10}; do
+#           echo "$pos,$ori,$act,$seed" >> params.csv
+#       done
+#   done
+# done
+
+# # Last two pairs with only euler
+# pos=("0.001" "0.0005" "0.0001")
+# for pair in "${pos[@]}"; do
+#   pos=$(echo $pair | cut -d' ' -f1)
+#   for act in pos_only; do
+#       for seed in {1..10}; do
+#           echo "$pos,$act,$seed" >> params.csv
+#       done
+#   done
+# done
 
