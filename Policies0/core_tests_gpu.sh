97e8d358982ec2870b1918603ff1bb7cc0d794bb
CatherineMAbraham <catherinemabraham@gmail.com>
2026-07-01 14:59:22 +0100

    testing 1 seed from 0.2mm

--- Policies0/core_tests_gpu.sh
+++ Policies0/core_tests_gpu.sh
@@ -6,8 +6,8 @@
 #SBATCH --ntasks=1            # 4 agents total
 #SBATCH --cpus-per-task=1      # 4 CPUs per agent
 #SBATCH --mem=20G              # 8GB RAM per agent
-#SBATCH --time=15:00:00
-#SBATCH --array=1-10
+#SBATCH --time=60:00:00
+#SBATCH --array=1
 #SBATCH --output=out_%A_%a.out
 #SBATCH --error=err_%A_%a.err
 
