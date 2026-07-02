163e942cebbe0d90f6ca9bd26af02f30278f785a
CatherineMAbraham <catherinemabraham@gmail.com>
2026-07-02 07:19:23 +0100

    add more timesteps

--- Policies0/td3_v0_curriculum.py
+++ Policies0/td3_v0_curriculum.py
@@ -162,7 +162,7 @@ def train(threshold_pos=0.001, threshold_ori=np.deg2rad(6), action_type='pos_onl
                                     deterministic=True, n_eval_episodes=100,callback_after_eval=success_callback,
                                     verbose=1)
 
-        model.learn(1_500_000, callback=eval_callback,reset_num_timesteps=False)
+        model.learn(500_000_000, callback=eval_callback,reset_num_timesteps=False)
     
         current_threshold_pos, current_threshold_ori = next_thresholds(current_threshold_pos, current_threshold_ori)
 
