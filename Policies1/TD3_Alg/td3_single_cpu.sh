37fa9c99413a8435ffb00905718d24c1efdbb1cd
CatherineMAbraham <catherinemabraham@gmail.com>
2026-07-09 13:37:06 +0100

    single cpu version to test spring

---
+++ Policies1/TD3_Alg/td3_single_cpu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --mail-user=cmabraham1@sheffield.ac.uk
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --ntasks=1            # 4 agents total
+#SBATCH --cpus-per-task=5      # 4 CPUs per agent
+#SBATCH --mem=20G              # 8GB RAM per agent
+#SBATCH --time=8:00:00
+#SBATCH --output=out_%A_%a.out
+
+
+module load Anaconda3/2024.02-1
+
+source activate softsurg
+# Read the correct line from params_curr_compare.csv
+
+# Run the script
+#srun --export=ALL 
+python td3_v1.py --threshold_pos 0.0005 --threshold_ori 0.5 --action_type euler --maxforce 3 --softtissue spring --contact_type 0 --render_mode 'direct' --num_springs 3 --log 1
