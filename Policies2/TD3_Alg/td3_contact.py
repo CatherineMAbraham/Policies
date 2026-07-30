9eca4cce6a1461c431ac2ebb55b10cbb66626860
CatherineMAbraham <catherinemabraham@gmail.com>
2026-07-30 20:41:58 +0100

    lower force now we have a lower contact threshold

--- Policies2/TD3_Alg/td3_contact.py
+++ Policies2/TD3_Alg/td3_contact.py
@@ -197,53 +197,53 @@ def train(threshold_pos=0.001,
         callback = [eval_callback]
     model.learn(1_000_000, callback=callback)
     #save model name in log file
-    with open('./logs/model_log.txt', 'w') as f:
-        f.write(f'{model_name}\n')
-    #model.save(f'./models/{model_name}')
-    #model.save_replay_buffer(f'./models/{model_name}-rb')
-    wandb.finish()  # Finish the W&B run
-    contact_env_kwargs = {
-            'reward_type': 'sparse',
-            'max_steps': 100,
-            'horizon': 'variable',
-            'obs_type': 'dict',
-            'distance_threshold_pos': threshold_pos,
-            'dt': 0.001,
-            'dr':0.01,
-            'distance_threshold_ori': threshold_ori,
-            'action_type': action_type,
-            'start_pos' : 'home',
-            'maxforce': maxforce,
-            'contact_type' :1,
-            'number_of_springs':num_springs,
-            'softtissue':softtissue,
-            'patient':110,
-            'test': False,
-            'youngs_modulus_type': youngs_modulus_type,
-            'render_mode': render_mode}
+    # with open('./logs/model_log.txt', 'w') as f:
+    #     f.write(f'{model_name}\n')
+    # #model.save(f'./models/{model_name}')
+    # #model.save_replay_buffer(f'./models/{model_name}-rb')
+    # wandb.finish()  # Finish the W&B run
+    # contact_env_kwargs = {
+    #         'reward_type': 'sparse',
+    #         'max_steps': 100,
+    #         'horizon': 'variable',
+    #         'obs_type': 'dict',
+    #         'distance_threshold_pos': threshold_pos,
+    #         'dt': 0.001,
+    #         'dr':0.01,
+    #         'distance_threshold_ori': threshold_ori,
+    #         'action_type': action_type,
+    #         'start_pos' : 'home',
+    #         'maxforce': maxforce,
+    #         'contact_type' :1,
+    #         'number_of_springs':num_springs,
+    #         'softtissue':softtissue,
+    #         'patient':110,
+    #         'test': False,
+    #         'youngs_modulus_type': youngs_modulus_type,
+    #         'render_mode': render_mode}
 
-    wandb.init(project="Chapter3-Test", name = (f'{softtissue}_{num_springs}_{youngs_modulus_name}_{seed}_{train_date}_contact'),notes= (f"Git Commit: {commit}"),sync_tensorboard=True, save_code=True)  # Initialize W&B
-    contact_env=make_vec_env('gym_fracture:anklesurg-v2', n_envs=1, env_kwargs=contact_env_kwargs, vec_env_cls=DummyVecEnv, seed = seed)
-    #contact_env = VecNormalize(contact_env, norm_obs=True, norm_reward=False)
-    contact_env = VecNormalize.load(f'./best_models/{ran}/{model_name}/vec_normalize.pkl', contact_env)
-    contact_env.training = True
-    contact_eval_env = make_vec_env('gym_fracture:anklesurg-v2', n_envs=1, env_kwargs=contact_env_kwargs, vec_env_cls=SubprocVecEnv, seed = eval_seed)
-    #contact_eval_env = VecNormalize(contact_eval_env, norm_obs=True, norm_reward=False)
-    contact_eval_env = VecNormalize.load(f'./best_models/{ran}/{model_name}/vec_normalize.pkl', contact_eval_env)
-    contact_eval_env.training = False
-    contact_success_callback = StopTrainingOnSuccessRate(vec_env=contact_eval_env, 
-                                                    max_no_improvement_evals=1,
-                                                    success_threshold=1,  
-                                                    min_evals=1, verbose=1, 
-                                                    model_name = model_name,
-                                                    model_save_path=f'./best_models/{ran}')
-    contact_eval_callback = EvalCallback(contact_eval_env,  eval_freq=10000,
-                                    deterministic=True, n_eval_episodes=100,
-                                    callback_after_eval=contact_success_callback)
-    model_path = f'./best_models/{ran}/{model_name}/{model_name}'
-    next_model = TD3.load(model_path, env=contact_env)
-    next_model.load_replay_buffer(f"{model_path}/{model_name}-rb")
-    next_model.learn(2_000_000, callback=contact_eval_callback,reset_num_timesteps=True, tb_log_name=f'{model_name}')
+    # wandb.init(project="Chapter3-Test", name = (f'{softtissue}_{num_springs}_{youngs_modulus_name}_{seed}_{train_date}_contact'),notes= (f"Git Commit: {commit}"),sync_tensorboard=True, save_code=True)  # Initialize W&B
+    # contact_env=make_vec_env('gym_fracture:anklesurg-v2', n_envs=1, env_kwargs=contact_env_kwargs, vec_env_cls=DummyVecEnv, seed = seed)
+    # #contact_env = VecNormalize(contact_env, norm_obs=True, norm_reward=False)
+    # contact_env = VecNormalize.load(f'./best_models/{ran}/{model_name}/vec_normalize.pkl', contact_env)
+    # contact_env.training = True
+    # contact_eval_env = make_vec_env('gym_fracture:anklesurg-v2', n_envs=1, env_kwargs=contact_env_kwargs, vec_env_cls=SubprocVecEnv, seed = eval_seed)
+    # #contact_eval_env = VecNormalize(contact_eval_env, norm_obs=True, norm_reward=False)
+    # contact_eval_env = VecNormalize.load(f'./best_models/{ran}/{model_name}/vec_normalize.pkl', contact_eval_env)
+    # contact_eval_env.training = False
+    # contact_success_callback = StopTrainingOnSuccessRate(vec_env=contact_eval_env, 
+    #                                                 max_no_improvement_evals=1,
+    #                                                 success_threshold=1,  
+    #                                                 min_evals=1, verbose=1, 
+    #                                                 model_name = model_name,
+    #                                                 model_save_path=f'./best_models/{ran}')
+    # contact_eval_callback = EvalCallback(contact_eval_env,  eval_freq=10000,
+    #                                 deterministic=True, n_eval_episodes=100,
+    #                                 callback_after_eval=contact_success_callback)
+    # model_path = f'./best_models/{ran}/{model_name}/{model_name}'
+    # next_model = TD3.load(model_path, env=contact_env)
+    # next_model.load_replay_buffer(f"{model_path}-rb")
+    # next_model.learn(2_000_000, callback=contact_eval_callback,reset_num_timesteps=True, tb_log_name=f'{model_name}')
 
 
 if __name__ == "__main__":
