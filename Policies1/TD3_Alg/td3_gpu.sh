e04ea0896db5d730bc75509c6a0d4fe73878c53f
CatherineMAbraham <catherinemabraham@gmail.com>
2026-08-14 18:51:24 +0100

    changed to new soft file as needed to remove eval

--- Policies1/TD3_Alg/td3_gpu.sh
+++ Policies1/TD3_Alg/td3_gpu.sh
@@ -18,7 +18,7 @@ source activate softsurg
 # Read the correct line from params_curr_compare.csv
 TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
 PARAM_LINE=$(sed -n "${TASK_ID}p" tests.csv)
-IFS=',' read -r TISSUE NUM_SPRINGS YOUNGS_MODULUS  SEED <<< "$PARAM_LINE"
-echo "Running test with: Tissue=$TISSUE, Young's Modulus=$YOUNGS_MODULUS, Number of Springs=$NUM_SPRINGS, Seed=$SEED"
+IFS=',' read -r TISSUE YOUNGS_MODULUS  SEED <<< "$PARAM_LINE"
+echo "Running test with: Tissue=$TISSUE, Young's Modulus=$YOUNGS_MODULUS, Seed=$SEED"
 # Run the script 
-srun --export=ALL python td3_v1.py --threshold_pos 0.0005 --threshold_ori 0.5 --action_type euler --maxforce 3 --num_springs $NUM_SPRINGS --softtissue $TISSUE --youngs_modulus $YOUNGS_MODULUS --contact_type 0 --seed $SEED --ran $TASK_ID --log 1
+srun --export=ALL python td3_v1_soft.py --threshold_pos 0.0005 --threshold_ori 0.5 --action_type euler --maxforce 3  --softtissue $TISSUE --youngs_modulus $YOUNGS_MODULUS --contact_type 0 --seed $SEED --ran $TASK_ID --log 1
