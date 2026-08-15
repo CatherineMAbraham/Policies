46c1c8ab50f66106c9c30aac398d64d19d4ea91b
CatherineMAbraham <catherinemabraham@gmail.com>
2026-08-15 12:39:33 +0100

    want to see if conda would work

--- Policies1/TD3_Alg/td3_gpu.sh
+++ Policies1/TD3_Alg/td3_gpu.sh
@@ -12,11 +12,12 @@
 #SBATCH --output=out_%A_%a.out
 
 
-PYTHON_EXEC="/users/cop21cma/.conda/envs/softsurg9/bin/python"
+#PYTHON_EXEC="/users/cop21cma/.conda/envs/softsurg9/bin/python"
+source activate softsurg9
 # Read the correct line from params_curr_compare.csv
 TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
 PARAM_LINE=$(sed -n "${TASK_ID}p" tests.csv)
 IFS=',' read -r TISSUE YOUNGS_MODULUS  SEED <<< "$PARAM_LINE"
 echo "Running test with: Tissue=$TISSUE, Young's Modulus=$YOUNGS_MODULUS, Seed=$SEED"
 # Run the script 
-srun --export=ALL $PYTHON_EXEC td3_v1_soft.py --threshold_pos 0.0005 --threshold_ori 0.5 --action_type euler --maxforce 3  --softtissue $TISSUE --youngs_modulus $YOUNGS_MODULUS --contact_type 0 --seed $SEED --ran $TASK_ID --log 1
+srun --export=ALL python td3_v1_soft.py --threshold_pos 0.0005 --threshold_ori 0.5 --action_type euler --maxforce 3  --softtissue $TISSUE --youngs_modulus $YOUNGS_MODULUS --contact_type 0 --seed $SEED --ran $TASK_ID --log 1
