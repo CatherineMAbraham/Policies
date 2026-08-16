f9d7a0220a17e7c70e0594691bcd359ba1ee1ebe
CatherineMAbraham <catherinemabraham@gmail.com>
2026-08-16 15:17:25 +0100

    forgot srun

--- Policies2/TD3_Alg/td3_gpu.sh
+++ Policies2/TD3_Alg/td3_gpu.sh
@@ -12,12 +12,11 @@
 
 
 PYTHON_EXEC="/users/cop21cma/.conda/envs/softsurg9/bin/python"
-source activate softsurg
+#source activate softsurg
 # Read the correct line from params_curr_compare.csv
 # TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
 # PARAM_LINE=$(sed -n "${TASK_ID}p" tests.csv)
 # IFS=',' read -r TISSUE NUM_SPRINGS YOUNGS_MODULUS  SEED <<< "$PARAM_LINE"
 # echo "Running test with: Tissue=$TISSUE, Young's Modulus=$YOUNGS_MODULUS, Number of Springs=$NUM_SPRINGS, Seed=$SEED"
 # Run the script 
-#srun --export=ALL $PYTHON_EXEC 
-python td3_soft.py --threshold_pos 0.0005 --threshold_ori 0.5 --action_type euler --maxforce 3 --num_springs 3 --softtissue spring --youngs_modulus_type testing --contact_type 0 --seed 1 --ran 1
+srun --export=ALL $PYTHON_EXEC td3_soft.py --threshold_pos 0.0005 --threshold_ori 0.5 --action_type euler --maxforce 3 --num_springs 3 --softtissue spring --youngs_modulus_type testing --contact_type 0 --seed 1 --ran 1
