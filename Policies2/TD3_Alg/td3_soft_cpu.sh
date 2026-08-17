7afe145a0a9a9bb7a4da0ba8e76ec255a6e794a2
CatherineMAbraham <catherinemabraham@gmail.com>
2026-08-17 12:17:01 +0100

    test these on cpu

--- Policies2/TD3_Alg/td3_soft_cpu.sh
+++ Policies2/TD3_Alg/td3_soft_cpu.sh
@@ -3,17 +3,16 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --ntasks=1            # 4 agents total
 #SBATCH --cpus-per-task=4      # 4 CPUs per agent
-#SBATCH --mem=8G              # 8GB RAM per agent
-#SBATCH --array=1-20
-#SBATCH --time=96:00:00
+#SBATCH --mem=80G              # 8GB RAM per agent
+#SBATCH --array=1-2
+#SBATCH --time=40:00:00
 #SBATCH --output=out_%A_%a.out
 
-module load Anaconda3/2024.02-1
+PYTHON_EXEC="/users/cop21cma/.conda/envs/softsurg9/bin/python"
 
-source activate softsurg
 TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
 PARAM_LINE=$(sed -n "${TASK_ID}p" tests_params.csv)
 IFS=',' read -r YM_TYPE CONTACT SEED <<< "$PARAM_LINE"
 echo "Running test with: Young's Modulus Type=$YM_TYPE, Contact Type=$CONTACT, Seed=$SEED"
 # Run the script
-srun --export=ALL python td3_soft.py --threshold_pos 0.0005 --threshold_ori 0.5 --action_type euler --maxforce 3 --softtissue spring --youngs_modulus_type $YM_TYPE --contact_type $CONTACT --seed $SEED --render_mode 'direct' --log 1 --ran $TASK_ID
+srun --export=ALL $PYTHON_EXEC td3_soft.py --threshold_pos 0.0005 --threshold_ori 0.5 --action_type euler --maxforce 4 --softtissue spring --youngs_modulus_type $YM_TYPE --contact_type $CONTACT --seed $SEED --render_mode 'direct' --log 1 --ran $TASK_ID
