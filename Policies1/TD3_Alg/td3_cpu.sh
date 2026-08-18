c6d9c75b64c0bf319e9057a0a5c784fca4d28b49
CatherineMAbraham <catherinemabraham@gmail.com>
2026-08-18 21:31:36 +0100

    i think python_exec was wrong

--- Policies1/TD3_Alg/td3_cpu.sh
+++ Policies1/TD3_Alg/td3_cpu.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=5      # 4 CPUs per agent
 #SBATCH --mem=80G              # 8GB RAM per agent
 #SBATCH --array=1-10
-#SBATCH --time=96:00:00
+#SBATCH --time=25:00:00
 #SBATCH --output=out_%A_%a.out
 
 
@@ -18,7 +18,7 @@ PARAM_LINE=$(sed -n "${TASK_ID}p" tests.csv)
 IFS=',' read -r TISSUE YOUNGS_MODULUS  SEED <<< "$PARAM_LINE"
 echo "Running test with: Tissue=$TISSUE, Young's Modulus=$YOUNGS_MODULUS, Seed=$SEED"
 # Run the script 
-srun --export=ALL $PYTHON_EXEC td3_v1.py \
+srun --export=ALL python td3_v1.py \
   --threshold_pos 0.0005 \
   --threshold_ori 0.5 \
   --action_type euler \
