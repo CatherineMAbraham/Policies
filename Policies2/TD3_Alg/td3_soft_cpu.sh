24387541dc82e9a949b3037ea3c6866f05b1fa39
CatherineMAbraham <catherinemabraham@gmail.com>
2026-08-18 20:40:44 +0100

    testing out v2 randomise and contact with new force

--- Policies2/TD3_Alg/td3_soft_cpu.sh
+++ Policies2/TD3_Alg/td3_soft_cpu.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1            # 4 agents total
 #SBATCH --cpus-per-task=4      # 4 CPUs per agent
 #SBATCH --mem=80G              # 8GB RAM per agent
-#SBATCH --array=1-2
+#SBATCH --array=1-10
 #SBATCH --time=40:00:00
 #SBATCH --output=out_%A_%a.out
 
@@ -12,7 +12,18 @@ PYTHON_EXEC="/users/cop21cma/.conda/envs/softsurg9/bin/python"
 
 TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
 PARAM_LINE=$(sed -n "${TASK_ID}p" tests_params.csv)
-IFS=',' read -r YM_TYPE FORCE SEED <<< "$PARAM_LINE"
+IFS=',' read -r YM_TYPE CONTACT SEED <<< "$PARAM_LINE"
 echo "Running test with: Young's Modulus Type=$YM_TYPE, Contact Type=$CONTACT, Seed=$SEED"
 # Run the script
-srun --export=ALL $PYTHON_EXEC td3_soft.py --threshold_pos 0.0005 --threshold_ori 0.5 --action_type euler --maxforce $FORCE --softtissue spring --youngs_modulus_type $YM_TYPE --contact_type 0 --seed $SEED --render_mode 'direct' --log 1 --ran $TASK_ID
+srun --export=ALL $PYTHON_EXEC td3_soft.py \
+--threshold_pos 0.0005 \
+--threshold_ori 0.5 \
+--action_type euler\
+ --maxforce 5 \
+ --softtissue spring\
+  --youngs_modulus_type $YM_TYPE \
+  --contact_type $CONTACT\
+   --seed $SEED\
+    --render_mode 'direct'\
+     --log 1 \
+     --ran $TASK_ID
