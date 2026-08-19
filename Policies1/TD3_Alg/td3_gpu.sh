ad65e1c12399181d16ddfaeabe36b985f4bcf5ad
CatherineMAbraham <catherinemabraham@gmail.com>
2026-08-19 16:57:42 +0100

    Force minimisation tests

--- Policies1/TD3_Alg/td3_gpu.sh
+++ Policies1/TD3_Alg/td3_gpu.sh
@@ -6,17 +6,29 @@
 #SBATCH --qos=gpu
 #SBATCH --ntasks=1            # 4 agents total
 #SBATCH --cpus-per-task=5      # 4 CPUs per agent
-#SBATCH --mem=10G              # 8GB RAM per agent
-#SBATCH --time=15:00:00
+#SBATCH --mem=80G              # 8GB RAM per agent
+#SBATCH --array=1-10
+#SBATCH --time=25:00:00
 #SBATCH --output=out_%A_%a.out
 
 
-PYTHON_EXEC="/users/cop21cma/.conda/envs/softsurg9/bin/python"
+PYTHON_EXEC="/mnt/parscratch/users/cop21cma/anaconda/.envs/softsurg9/bin/python"
 #source activate softsurg9
 # Read the correct line from params_curr_compare.csv
-# TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
-# PARAM_LINE=$(sed -n "${TASK_ID}p" tests.csv)
-# IFS=',' read -r TISSUE YOUNGS_MODULUS  SEED <<< "$PARAM_LINE"
+TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
+PARAM_LINE=$(sed -n "${TASK_ID}p" tests.csv)
+IFS=',' read -r TISSUE YOUNGS_MODULUS  SEED <<< "$PARAM_LINE"
 # echo "Running test with: Tissue=$TISSUE, Young's Modulus=$YOUNGS_MODULUS, Seed=$SEED"
 # Run the script 
-srun --export=ALL $PYTHON_EXEC td3_v1.py --threshold_pos 0.0005 --threshold_ori 0.5 --action_type euler --maxforce 5 --softtissue spring --num_springs 3 --youngs_modulus 5e5 --contact_type 0 --seed 5 --log 1 --render_mode 'direct'
+srun --export=ALL $PYTHON_EXEC td3_v1.py \
+  --threshold_pos 0.0005 \
+  --threshold_ori 0.5 \
+  --action_type euler \
+  --maxforce 5 \
+  --softtissue $TISSUE \
+  --num_springs 1 \
+  --youngs_modulus $YOUNGS_MODULUS \
+  --contact_type 0 \
+  --seed $SEED \
+  --log 1 \
+  --render_mode 'direct'
