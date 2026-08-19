1408cf2670d37ae0258270aa562bdb65dd3e8263
CatherineMAbraham <catherinemabraham@gmail.com>
2026-08-19 11:06:53 +0100

    run on cpu

--- Policies1/TD3_Alg/td3_test.sh
+++ Policies1/TD3_Alg/td3_test.sh
@@ -2,7 +2,7 @@
 #SBATCH --mail-user=cmabraham1@sheffield.ac.uk
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --ntasks=1            # 4 agents total
-#SBATCH --cpus-per-task=10     # 4 CPUs per agent
+#SBATCH --cpus-per-task=5      # 4 CPUs per agent
 #SBATCH --mem=80G              # 8GB RAM per agent
 #SBATCH --array=1-10
 #SBATCH --time=25:00:00
@@ -19,7 +19,10 @@ PARAM_LINE=$(sed -n "${TASK_ID}p" model_log.csv)
 IFS=',' read -r MODEL SEED<<< "$PARAM_LINE"
 echo "Running test with: Model=$MODEL"
 # Run the script 
-srun --export=ALL python td3_v1.py \
+srun --export=ALL $PYTHON_EXEC td3_v1.py \
+ --threshold_pos 0.0005 \
+  --threshold_ori 0.5 \
+  --maxforce 4\
  --model $MODEL \
  --seed $SEED \
   --log 1 \
