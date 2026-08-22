8ec00a47e0708dfa20bf858df8fdff019a893ff3
CatherineMAbraham <catherinemabraham@gmail.com>
2026-08-22 14:36:48 +0100

    test it with no contact

--- Policies2/TD3_Alg/td3_single.sh
+++ Policies2/TD3_Alg/td3_single.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1            # 4 agents total
 #SBATCH --cpus-per-task=5      # 4 CPUs per agent
 #SBATCH --mem=80G              # 8GB RAM per agent
-#SBATCH --time=20:00:00
+#SBATCH --time=30:00:00
 #SBATCH --output=out_%A_%a.out
 
 
@@ -15,7 +15,7 @@ source activate softsurg9
 
 # Run the script
 #srun --export=ALL 
-python td3_test.py \
+python td3_soft.py \
     --threshold_pos 0.0005 \
     --threshold_ori 0.5 \
     --action_type euler \
@@ -23,8 +23,9 @@ python td3_test.py \
     --num_springs 3 \
     --youngs_modulus 5e5 \
     --softtissue spring \
-    --render_mode 'human' \
+    --render_mode 'direct' \
     --youngs_modulus_type testing \
-    --contact_type 1 \
+    --randomise_num_springs 1\
+    --contact_type 0 \
     --seed 1 \
     --ran 1
