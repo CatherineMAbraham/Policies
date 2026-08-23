939850aefa4257e045e18c5e23a4379b8ac34aa1
CatherineMAbraham <catherinemabraham@gmail.com>
2026-08-23 20:26:58 +0100

    ym random test

---
+++ Policies2/TD3_Alg/td3_gpu_random.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --mail-user=cmabraham1@sheffield.ac.uk
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:a100:1
+#SBATCH --qos=gpu
+#SBATCH --ntasks=1            # 4 agents total
+#SBATCH --cpus-per-task=5     # 4 CPUs per agent
+#SBATCH --array=1-10
+#SBATCH --mem=30G              # 8GB RAM per agent
+#SBATCH --time=25:00:00
+#SBATCH --output=out_%A_%a.out
+
+
+PYTHON_EXEC="/users/cop21cma/.conda/envs/softsurg9/bin/python"
+#source activate softsurg
+# Read the correct line from params_curr_compare.csv
+TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
+PARAM_LINE=$(sed -n "${TASK_ID}p" tests_params.csv)
+IFS=',' read -r CONTACT  SEED <<< "$PARAM_LINE"
+srun --export=ALL $PYTHON_EXEC td3_soft.py \
+                --threshold_pos 0.0005 \
+                --threshold_ori 0.5 \
+                --action_type euler \
+                --maxforce 5 \
+                --num_springs 3 \
+                --youngs_modulus 5e5 \
+                --maximum_contact_force_threshold 0.25 \
+                --softtissue spring \
+                --render_mode 'direct' \
+                --youngs_modulus_type testing \
+                --randomise_num_springs 1 \
+                --contact_type 1 \
+                --seed $SEED \
+                --ran $TASK_ID
+
