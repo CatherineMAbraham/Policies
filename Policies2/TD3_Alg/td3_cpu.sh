a1bf25b7c226e7a9dbc5d7e77a3b857ef3c27b26
CatherineMAbraham <catherinemabraham@gmail.com>
2026-08-24 20:52:18 +0100

    had python in there

--- Policies2/TD3_Alg/td3_cpu.sh
+++ Policies2/TD3_Alg/td3_cpu.sh
@@ -17,7 +17,7 @@ PARAM_LINE=$(sed -n "${TASK_ID}p" tests_params.csv)
 IFS=',' read -r CONTACT  SEED <<< "$PARAM_LINE"
 #echo "Running test with: Contact=$CONTACT, Contact Force=$CONTACTFORCE, Seed=$SEED"
 #Run the script 
-srun --export=ALL $PYTHON_EXEC python td3_soft.py \
+srun --export=ALL $PYTHON_EXEC td3_soft.py \
                 --threshold_pos 0.0005 \
                 --threshold_ori 0.5 \
                 --action_type euler \
