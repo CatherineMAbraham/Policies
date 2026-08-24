4ecd53d6792593da92f7ef2d32916fb3ad1e1c91
CatherineMAbraham <catherinemabraham@gmail.com>
2026-08-24 12:58:00 +0100

    update gpu

--- Policies2/Sweep/sweep.sh
+++ Policies2/Sweep/sweep.sh
@@ -2,18 +2,22 @@
 #SBATCH --mail-user=cmabraham1@sheffield.ac.uk
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --partition=gpu
+#SBATCH --gres=gpu:a100:1
 #SBATCH --qos=gpu
-#SBATCH --gres=gpu:1
-#SBATCH --ntasks=10            # 4 agents total
-#SBATCH --cpus-per-task=1      # 4 CPUs per agent
-#SBATCH --mem=80G              # 8GB RAM per agent
-#SBATCH --time=40:00:00
+#SBATCH --ntasks=20
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=20G
+#SBATCH --time=48:00:00
+#SBATCH --output=out_%A_%a.out
 
+# Activate Conda Environment properly
+source ~/.bashrc
+conda activate softsurg9
 
+# (Optional) Export W&B API key if needed
+# export WANDB_API_KEY="your_api_key_here"
 
-module load Anaconda3/2024.02-1
+# Execute agent script with the generated Sweep ID
+SWEEP_ID="v8avoer6"
 
-source activate softsurg
-wandb init --entity cmabraham1-university-of-sheffield --project Chp2-Sweep
-# Run the script 
-srun --export=ALL python td3_sweep.py 
+python td3_sweep_v2.py --sweep_id $SWEEP_ID --count 5
