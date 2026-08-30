fbd39a6029424021c55fb741cc4aff3b63f572a7
CatherineMAbraham <catherinemabraham@gmail.com>
2026-08-30 21:03:18 +0100

    cpu test should be right now

--- Policies2/TD3_Alg/td3_cpu.sh
+++ Policies2/TD3_Alg/td3_cpu.sh
@@ -10,15 +10,14 @@
 
 
 PYTHON_EXEC="/users/cop21cma/.conda/envs/softsurg9/bin/python"
-source activate softsurg
+#source activate softsurg
 # Read the correct line from params_curr_compare.csv
 TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
 #PARAM_LINE=$(sed -n "${TASK_ID}p" tests_params.csv)
 #IFS=',' read -r CONTACT CONTACTFORCE SEED <<< "$PARAM_LINE"
 #echo "Running test with: Contact=$CONTACT, Contact Force=$CONTACTFORCE, Seed=$SEED"
 #Run the script 
-#srun --export=ALL $PYTHON_EXEC 
-python td3_soft.py \
+srun --export=ALL $PYTHON_EXEC python td3_soft.py \
                 --threshold_pos 0.0005 \
                 --threshold_ori 0.5 \
                 --action_type euler \
