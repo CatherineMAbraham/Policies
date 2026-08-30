629a8df46798649e10df2baa93f118188f0d3647
CatherineMAbraham <catherinemabraham@gmail.com>
2026-08-30 09:15:11 +0100

    testing contact at 0.1

--- Policies2/TD3_Alg/td3_gpu.sh
+++ Policies2/TD3_Alg/td3_gpu.sh
@@ -6,9 +6,8 @@
 #SBATCH --qos=gpu
 #SBATCH --ntasks=1            # 4 agents total
 #SBATCH --cpus-per-task=5     # 4 CPUs per agent
-#SBATCH --array=1-20
 #SBATCH --mem=30G              # 8GB RAM per agent
-#SBATCH --time=25:00:00
+#SBATCH --time=15:00:00
 #SBATCH --output=out_%A_%a.out
 
 
@@ -16,20 +15,20 @@ PYTHON_EXEC="/users/cop21cma/.conda/envs/softsurg9/bin/python"
 #source activate softsurg
 # Read the correct line from params_curr_compare.csv
 TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
-PARAM_LINE=$(sed -n "${TASK_ID}p" tests_params.csv)
-IFS=',' read -r CONTACT  SEED <<< "$PARAM_LINE"
+#PARAM_LINE=$(sed -n "${TASK_ID}p" tests_params.csv)
+#IFS=',' read -r CONTACT  SEED <<< "$PARAM_LINE"
 srun --export=ALL $PYTHON_EXEC td3_soft.py \
-    --threshold_pos 0.0005 \
-    --threshold_ori 0.5 \
-    --action_type euler \
-    --maxforce 5 \
-    --num_springs 3 \
-    --youngs_modulus 5e5 \
-    --maximum_contact_force_threshold 0.25 \
-    --softtissue spring \
-    --render_mode 'direct' \
-    --youngs_modulus_type None \
-    --contact_type $CONTACT \
-    --seed $SEED \
-    --ran $TASK_ID
+                --threshold_pos 0.0005 \
+                --threshold_ori 0.5 \
+                --action_type euler \
+                --maxforce 5 \
+                --num_springs 3 \
+                --youngs_modulus 5e5 \
+                --maximum_contact_force_threshold 0.1 \
+                --softtissue spring \
+                --render_mode 'direct' \
+                --youngs_modulus_type None \
+                --contact_type 1 \
+                --seed 1 \
+                --ran 1
 
