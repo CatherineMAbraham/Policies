131be38af540da7b569e70ec5e01fe33045154bb
CatherineMAbraham <catherinemabraham@gmail.com>
2026-09-02 19:58:16 +0100

    just testing random ym

--- Policies2/TD3_Alg/td3_cpu_random.sh
+++ Policies2/TD3_Alg/td3_cpu_random.sh
@@ -3,14 +3,13 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --ntasks=1            # 4 agents total
 #SBATCH --cpus-per-task=5     # 4 CPUs per agent
-#SBATCH --array=1-20
 #SBATCH --mem=30G              # 8GB RAM per agent
 #SBATCH --time=25:00:00
 #SBATCH --output=out_%A_%a.out
 
 
 #PYTHON_EXEC="/users/cop21cma/.conda/envs/softsurg9/bin/python"
-source activate softsurg
+source activate softsurg9
 # Read the correct line from params_curr_compare.csv
 TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
 #PARAM_LINE=$(sed -n "${TASK_ID}p" tests_params.csv)
@@ -26,10 +25,11 @@ python td3_soft.py \
                 --maximum_contact_force_threshold 1.0 \
                 --softtissue spring \
                 --render_mode 'direct' \
-                --youngs_modulus_type None \
-                --randomise_foot_dynamics 1\
+                --youngs_modulus_type testing \
+                --randomise_foot_dynamics 0\
                 --randomise_num_springs 0 \
                 --contact_type 1 \
                 --seed 1 \
+                --log 0\
                 --ran 1
 
