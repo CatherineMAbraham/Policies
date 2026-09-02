ca294c88ea747cf59048e1a2afb7b105f6ea3a40
CatherineMAbraham <catherinemabraham@gmail.com>
2026-09-02 19:50:22 +0100

    random tests

--- Policies2/TD3_Alg/td3_cpu_random.sh
+++ Policies2/TD3_Alg/td3_cpu_random.sh
@@ -9,24 +9,26 @@
 #SBATCH --output=out_%A_%a.out
 
 
-PYTHON_EXEC="/users/cop21cma/.conda/envs/softsurg9/bin/python"
-#source activate softsurg
+#PYTHON_EXEC="/users/cop21cma/.conda/envs/softsurg9/bin/python"
+source activate softsurg
 # Read the correct line from params_curr_compare.csv
 TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
-PARAM_LINE=$(sed -n "${TASK_ID}p" tests_params.csv)
-IFS=',' read -r CONTACT_THRESHOLD SEED <<< "$PARAM_LINE"
-srun --export=ALL $PYTHON_EXEC td3_soft.py \
+#PARAM_LINE=$(sed -n "${TASK_ID}p" tests_params.csv)
+#IFS=',' read -r CONTACT_THRESHOLD SEED <<< "$PARAM_LINE"
+#srun --export=ALL $PYTHON_EXEC 
+python td3_soft.py \
                 --threshold_pos 0.0005 \
                 --threshold_ori 0.5 \
                 --action_type euler \
                 --maxforce 5 \
                 --num_springs 3 \
                 --youngs_modulus 5e5 \
-                --maximum_contact_force_threshold $CONTACT_THRESHOLD \
+                --maximum_contact_force_threshold 1.0 \
                 --softtissue spring \
                 --render_mode 'direct' \
                 --youngs_modulus_type testing \
-                --randomise_num_springs 1 \
+                --randomise_foot_dynamics 1\
+                --randomise_num_springs 0 \
                 --contact_type 1 \
                 --seed $SEED \
                 --ran $TASK_ID
