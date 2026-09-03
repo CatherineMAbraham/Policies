1c42e4ec1898110cd2dda7029164c70426f4acc1
CatherineMAbraham <catherinemabraham@gmail.com>
2026-09-03 17:03:05 +0100

    all randomisation on

--- Policies2/TD3_Alg/td3_cpu_random.sh
+++ Policies2/TD3_Alg/td3_cpu_random.sh
@@ -14,19 +14,20 @@ PYTHON_EXEC="/users/cop21cma/.conda/envs/softsurg9/bin/python"
 TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
 #PARAM_LINE=$(sed -n "${TASK_ID}p" tests_params.csv)
 #IFS=',' read -r CONTACT_THRESHOLD SEED <<< "$PARAM_LINE"
-srun --export=ALL $PYTHON_EXEC  td3_soft.py \
+#srun --export=ALL $PYTHON_EXEC  
+python td3_soft.py \
                 --threshold_pos 0.0005 \
                 --threshold_ori 0.5 \
                 --action_type euler \
                 --maxforce 5 \
                 --num_springs 3 \
                 --youngs_modulus 5e5 \
-                --maximum_contact_force_threshold 1.0 \
+                --maximum_contact_force_threshold 0.5 \
                 --softtissue spring \
                 --render_mode 'direct' \
                 --youngs_modulus_type testing \
                 --randomise_foot_dynamics 1\
-                --randomise_num_springs 0 \
+                --randomise_num_springs 1 \
                 --randomise_sensor_noise 1\
                 --contact_type 1 \
                 --seed 1 \
