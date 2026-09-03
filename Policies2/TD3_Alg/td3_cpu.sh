90be38388a96ce864eafc983e50500543b3a289c
CatherineMAbraham <catherinemabraham@gmail.com>
2026-09-03 15:12:49 +0100

    test it with 5mm and 0.5 deg too

--- Policies2/TD3_Alg/td3_cpu.sh
+++ Policies2/TD3_Alg/td3_cpu.sh
@@ -2,8 +2,8 @@
 #SBATCH --mail-user=cmabraham1@sheffield.ac.uk
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --ntasks=1            # 4 agents total
-#SBATCH --cpus-per-task=5    # 4 CPUs per agent
-#SBATCH --mem=40G              # 8GB RAM per agent
+#SBATCH --cpus-per-task=1    # 4 CPUs per agent
+#SBATCH --mem=20G              # 8GB RAM per agent
 #SBATCH --array=1-25
 #SBATCH --time=15:00:00
 #SBATCH --output=out_%A_%a.out
@@ -19,8 +19,8 @@ IFS=',' read -r CONTACTFORCE SEED <<< "$PARAM_LINE"
 #echo "Running test with: Contact=$CONTACT, Contact Force=$CONTACTFORCE, Seed=$SEED"
 #Run the script 
 srun --export=ALL $PYTHON_EXEC td3_soft.py \
-                --threshold_pos 0.0002 \
-                --threshold_ori 0.2 \
+                --threshold_pos 0.0005 \
+                --threshold_ori 0.5 \
                 --action_type euler \
                 --maxforce 5 \
                 --num_springs 3 \
