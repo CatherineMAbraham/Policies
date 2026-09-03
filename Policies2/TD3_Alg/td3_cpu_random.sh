e018af1cd5691d1295b6b323806dbb867b70f63b
CatherineMAbraham <catherinemabraham@gmail.com>
2026-09-03 12:51:30 +0100

    need to test on hpc

--- Policies2/TD3_Alg/td3_cpu_random.sh
+++ Policies2/TD3_Alg/td3_cpu_random.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 #SBATCH --mail-user=cmabraham1@sheffield.ac.uk
 #SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:a100:1
+#SBATCH --qos=gpu
 #SBATCH --ntasks=1            # 4 agents total
 #SBATCH --cpus-per-task=5     # 4 CPUs per agent
-#SBATCH --mem=30G              # 8GB RAM per agent
-#SBATCH --time=25:00:00
+#SBATCH --mem=20G              # 8GB RAM per agent
+#SBATCH --time=15:00:00
 #SBATCH --output=out_%A_%a.out
 
 
-#PYTHON_EXEC="/users/cop21cma/.conda/envs/softsurg9/bin/python"
-source activate softsurg
+PYTHON_EXEC="/users/cop21cma/.conda/envs/softsurg9/bin/python"
+#source activate softsurg
 # Read the correct line from params_curr_compare.csv
 TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
 #PARAM_LINE=$(sed -n "${TASK_ID}p" tests_params.csv)
 #IFS=',' read -r CONTACT_THRESHOLD SEED <<< "$PARAM_LINE"
-#srun --export=ALL $PYTHON_EXEC 
-python td3_soft.py \
+srun --export=ALL $PYTHON_EXEC python td3_soft.py \
                 --threshold_pos 0.0005 \
                 --threshold_ori 0.5 \
                 --action_type euler \
