8c8d33fa5850b372eaf496c424a9e1bbcd73a31d
CatherineMAbraham <catherinemabraham@gmail.com>
2026-09-04 15:25:24 +0100

    random tests

--- Policies2/TD3_Alg/td3_cpu_random.sh
+++ Policies2/TD3_Alg/td3_cpu_random.sh
@@ -4,7 +4,8 @@
 #SBATCH --ntasks=1            # 4 agents total
 #SBATCH --cpus-per-task=5     # 4 CPUs per agent
 #SBATCH --mem=20G              # 8GB RAM per agent
-#SBATCH --time=15:00:00
+#SBATCH --array=1-5
+#SBATCH --time=25:00:00
 #SBATCH --output=out_%A_%a.out
 
 
@@ -14,24 +15,23 @@ source activate softsurg
 TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
 #PARAM_LINE=$(sed -n "${TASK_ID}p" tests_params.csv)
 #IFS=',' read -r CONTACT_THRESHOLD SEED <<< "$PARAM_LINE"
-#srun --export=ALL $PYTHON_EXEC  
-python td3_soft.py \
+srun --export=ALL $PYTHON_EXEC  python td3_soft.py \
                 --threshold_pos 0.0005 \
                 --threshold_ori 0.5 \
                 --action_type euler \
                 --maxforce 5 \
                 --num_springs 3 \
                 --youngs_modulus 5e5 \
-                --maximum_contact_force_threshold 0.5 \
+                --maximum_contact_force_threshold 0.3 \
                 --softtissue spring \
-                --render_mode 'direct' \
+                --render_mode 'human' \
                 --youngs_modulus_type testing \
                 --randomise_foot_dynamics 1\
                 --randomise_num_springs 1 \
                 --randomise_sensor_noise 1\
-                --randomise_start 1\
+                --randomise_start 0\
                 --contact_type 1 \
-                --seed 1 \
+                --seed $TASK_ID \
                 --log 1\
-                --ran 1
+                --ran $TASK_ID \
 
