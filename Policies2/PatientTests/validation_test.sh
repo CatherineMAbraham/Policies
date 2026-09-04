ba5f9c8a7a719949fd95d18f3b4d10beb8692c9f
CatherineMAbraham <catherinemabraham@gmail.com>
2026-09-04 12:43:03 +0100

    non random model

--- Policies2/PatientTests/validation_test.sh
+++ Policies2/PatientTests/validation_test.sh
@@ -2,7 +2,7 @@ source activate softsurg
 # Read the correct line from params_curr_compare.csv
 
 python validation_tests_random.py \
-                --model_path '/home/catherineabraham/Policies2/Policies2/TD3_Alg/contact/1/model-spring_randomYM_09031956_1' \
+                --model_path '/home/catherineabraham/Policies2/Policies2/TD3_Alg/contact/1/model-spring_contact_0.0005_09031515_1' \
                 --log 1\
                 --safemode 1\
                 --seed 11
